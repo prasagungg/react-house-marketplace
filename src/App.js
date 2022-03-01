5ce1e7106c42ef54ef74c6e5435051998e24149d
prasagungg <agungprasetio878@gmail.com>
2022-03-01 07:26:11 +0700

    route & pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//page
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
-      <h1>Oke</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/profie" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-up" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
